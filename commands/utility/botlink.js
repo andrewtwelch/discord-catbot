7cfd74e1227a56640833b0fe210a8b1709d6bdc3
Andrew Welch <mail@andrewwelch.net>
2024-03-21 10:52:45 +0000

    feat: add scope and permissions to bot link

--- commands/utility/botlink.js
+++ commands/utility/botlink.js
@@ -6,7 +6,7 @@ module.exports = {
         .setName('botlink')
         .setDescription(`Get a link to add the bot to your server!`),
     async execute(interaction) {
-        const botLink = `https://discord.com/oauth2/authorize?client_id=${clientId}`;
+        const botLink = `https://discord.com/oauth2/authorize?client_id=${clientId}&scope=applications.commands%20bot&permissions=18432`;
         const botLinkEmbed = new EmbedBuilder()
         .setTitle("Add CatBot to your server!")
         .setURL(botLink);
